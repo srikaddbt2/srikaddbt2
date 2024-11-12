935f6cb87dc1550a0f8f0e0c92205c7098d2977d
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-12 21:33:44 +0000

    KK

--- models/example/Record_count.sql
+++ models/example/Record_count.sql
@@ -9,8 +9,9 @@
 WITH row_counts AS (
   SELECT 
     'my_first_dbt_model' AS table_name,
-    get_row_count(ref('my_first_dbt_model'))  AS row_count
+    get_row_count({{ ref('my_first_dbt_model') }}  AS row_count
 
-)
+))
 
 SELECT * FROM row_counts
+
