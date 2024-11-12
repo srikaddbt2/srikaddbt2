21755c5e07378588b0eec85e1173bbfd1376a1a7
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-12 21:46:46 +0000

    ;

--- models/example/Record_count.sql
+++ models/example/Record_count.sql
@@ -6,7 +6,7 @@
 
 {% set row_count = get_row_count('my_first_dbt_model') %}
 -- You can then use this row count in your analysis or testing logic
- {{ row_count }} AS row_count;
+ select {{ row_count }} AS row_count;
 
 
 
