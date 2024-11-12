fb758e67193a35aac18a917292c44851080174af
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-12 21:36:17 +0000

    KK

--- models/example/Record_count.sql
+++ models/example/Record_count.sql
@@ -9,7 +9,7 @@
 WITH row_counts AS (
   SELECT 
     'my_first_dbt_model' AS table_name,
-    get_row_count({{ ref('my_first_dbt_model') }}  AS row_count
+    get_row_count({{ ref('my_first_dbt_model') }}  AS row_count;
 
 ))
 
