dccad1074253083f32ae6705c8f1951c9132aa24
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-13 01:28:13 +0000

    k

--- models/example/Record_count.sql
+++ models/example/Record_count.sql
@@ -5,8 +5,7 @@
 }}
 
 
-    select  get_row_count('{{ ref('my_first_dbt_model') }}') as count
-
-
+    select  {{ ref('my_first_dbt_model') }} AS table_name
+    {{ get_row_count(ref('my_first_dbt_model')) }} AS row_count
 
 
