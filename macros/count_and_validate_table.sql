8eef87a67a3c1f165071d221a9f8812a5e55f39b
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-13 22:47:12 +0000

    ll

---
+++ macros/count_and_validate_table.sql
@@ -0,0 +1,18 @@
+{% macro count_rows(table_name) %}
+  {% if execute %}
+    {% set table_exists_query %}
+      select 1 from {{ ref(table_name) }} limit 1
+    {% endset %}
+    
+    {% set table_exists = run_query(table_exists_query) %}
+    
+    {% if table_exists %}
+      select count(*) from {{ ref(table_name) }}
+    {% else %}
+      select 0 as count
+    {% endif %}
+  {% else %}
+    -- return a default value for non-executing contexts like tests
+    select 0 as count
+  {% endif %}
+{% endmacro %}
