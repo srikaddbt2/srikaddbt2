0245c29db75a8c34508087a9d015f17c69827b3e
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-13 01:55:05 +0000

    d

--- models/example/Record_count.sql
+++ models/example/Record_count.sql
@@ -5,8 +5,11 @@
 }}
 
 
-    select     get_row_count('{{ ref('my_first_dbt_model') }}') AS cnt
 
-    
+with row_count_cte as (
+    {{ get_row_count('my_first_dbt_model') }}  -- Call the macro and pass the table name
+)
 
-
+select
+    row_count
+from row_count_cte
