adf7051fa1b02c31b78aa4563cd14e9738919cc1
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-13 22:26:34 +0000

    s

--- macros/get_row_count1.sql
+++ macros/get_row_count1.sql
@@ -1,8 +1,4 @@
-/*{% macro get_row_count(table_name) %}
-  
-    select count(*) as row_count from {{ ref(table_name) }}
- 
-{% endmacro %}*/
+
 
 {% macro get_row_count(table_name) %}
   {% if execute %}
