dba9149b4a8c7b963873ad604c37f25216ae628e
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-13 02:57:15 +0000

    K

--- macros/grant_access.sql
+++ macros/grant_access.sql
@@ -1,14 +1,20 @@
 {% macro grant_access(role, database, schema, tables) %}
-    {% set grant_database = "GRANT USAGE ON DATABASE " ~ database ~ " TO ROLE " ~ role %}
-    {% set grant_schema = "GRANT USAGE ON SCHEMA " ~ database ~ "." ~ schema ~ " TO ROLE " ~ role %}
-    
-    {% for table in tables %}
-        {% set grant_table = "GRANT SELECT ON TABLE " ~ database ~ "." ~ schema ~ "." ~ table ~ " TO ROLE " ~ role %}
-        {% do run_query(grant_table) %}
-    {% endfor %}
-    
-    -- Grant database and schema access
-    {% do run_query(grant_database) %}
-    {% do run_query(grant_schema) %}
- 
+
+-- Grant usage on database
+GRANT USAGE ON DATABASE {{ database }} TO ROLE {{ role }};
+
+-- Grant usage on schema
+GRANT USAGE ON SCHEMA {{ database }}.{{ schema }} TO ROLE {{ role }};
+
+-- Grant select on all tables in schema
+GRANT SELECT ON ALL TABLES IN SCHEMA {{ database }}.{{ schema }} TO ROLE {{ role }};
+
+-- Grant select on future tables in schema
+GRANT SELECT ON FUTURE TABLES IN SCHEMA {{ database }}.{{ schema }} TO ROLE {{ role }};
+
+-- Grant select on specific tables
+{% for table in tables %}
+    GRANT SELECT ON TABLE {{ database }}.{{ schema }}.{{ table }} TO ROLE {{ role }};
+{% endfor %}
+
 {% endmacro %}
