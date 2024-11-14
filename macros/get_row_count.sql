d50166d6436fbec812f8994be9221f6a51bc8d50
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-14 00:16:34 +0000

    ll

--- macros/get_row_count.sql
+++ macros/get_row_count.sql
@@ -1,5 +1,5 @@
 {% macro get_row_count(table_name) %}
   
-    select count(*) as row_count from {{ ref(table_name) }}
+    select count(*) as row_count,{{ ref(table_name) }} as table from {{ ref(table_name) }}
  
 {% endmacro %}
--- models/example/Record_count.sql
+++ models/example/Record_count.sql
@@ -7,9 +7,9 @@
 
 
 with row_count_cte as (
-    {{ get_row_count('my_second_dbt_model') }} 
+    {{ get_row_count('my_second_dbt_model') }}
 )
 
 select
-    row_count
+    *
 from row_count_cte
