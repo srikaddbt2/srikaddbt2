0ef5a1b0a5e879fbaff94dc1873b4128f5f999d0
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-15 01:39:13 +0000

    ;;

--- models/example/Incremental_append.sql
+++ models/example/Incremental_append.sql
@@ -2,13 +2,13 @@
 config(
 materialized='incremental',
 unique_key='empid',
-alias='EMP_TRG11_DEL_INS',
+alias='EMP_TRG11_DEL_INS2',
 incremental_strategy='delete+insert'
  )
 }}
 
 select
-*,'{{invocation_id}}' as run_id, from DEV.RAW_Schema.EMP_STG
+*,'{{invocation_id}}' as run_id, from DEV.RAW_Schema.EMP_STG2
 {% if is_incremental() %}
 where inserted_at > (select max(inserted_at) from {{this}} )
 {% endif %}
