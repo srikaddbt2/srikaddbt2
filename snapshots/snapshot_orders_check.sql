bfa641f845734f8f7a96a4d704898eb9d87b06d8
srikaddbt2@gmail.com <srikaddbt2@gmail.com>
2024-11-15 02:28:38 +0000

    dd

--- snapshots/snapshot_orders_check.sql
+++ snapshots/snapshot_orders_check.sql
@@ -3,14 +3,14 @@
 {{
     config(
       target_database='DEV',
-      target_schema='CURATED_DATA',
-      unique_key='PROD_id',
+      target_schema='INT_SCHEMA',
+      unique_key='prod_id',
       strategy='check',
-      check_cols=['PROD_NAME','UNIT_PRICE'],
+      check_cols=['prod_name','unit_price'],
       invalidate_hard_deletes=True
     )
 }}
 
-select * from DEV.CURATED_DATA.PRODUCTS
+select * from DEV.RAW_SCHEMA.PROD_RAW
 
 {% endsnapshot %}
